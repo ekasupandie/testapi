b40711a40847b274e8990f0a8eb43b84b2ba4423
ekasupandie <eka.supandi@gmail.com>
2021-05-07 15:35:04 +0700

    Add files via upload

---
+++ precache-manifest.2c62aa42ed279d8f06834d4fd123b785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfbd459684b0f5aaca95983f991d2fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2c76142c33dd1fcd7cd",
+    "url": "/static/css/main.a6b81b63.chunk.css"
+  },
+  {
+    "revision": "fff15ece25505713aefd",
+    "url": "/static/js/2.4ade8ca2.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/static/js/2.4ade8ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c76142c33dd1fcd7cd",
+    "url": "/static/js/main.1fb3ce4c.chunk.js"
+  },
+  {
+    "revision": "40de54be25d0b6a47464",
+    "url": "/static/js/runtime-main.3337e351.js"
+  }
+]);
